81f86f4f9a07000fdf64622ffb2ca18476b6e36b
frusaka <fredrick.rusaka@gmail.com>
2025-02-05 10:58:11 -0600

    Add local storage auto-save

--- ui/index.js
+++ ui/index.js
@@ -1,5 +1,5 @@
 const studentList = JSON.parse(localStorage.getItem("student-list") || "[]");
-const DEFAULT_SIZE = 6;
+
 document
   .getElementById("new-student-name")
   .addEventListener("keyup", (event) =>
@@ -8,12 +8,13 @@ document
 document
   .getElementById("js-add-student-button")
   .addEventListener("click", addStudent);
+
 renderStudentList();
 
 function addStudent() {
   const studentName = document.getElementById("new-student-name").value;
   document.getElementById("new-student-name").value = "";
-  studentList.push({
+  studentList.unshift({
     name: studentName,
     requests: [""],
     exclude: [""],
@@ -22,7 +23,7 @@ function addStudent() {
   renderStudentList();
 }
 
-function removeTodo(index) {
+function removeStudent(index) {
   studentList.splice(index, 1);
   localStorage.setItem("student-list", JSON.stringify(studentList));
   renderStudentList();
@@ -32,7 +33,7 @@ function renderStudentList() {
   let todoHTML = "";
   studentList.forEach((student) => {
     todoHTML += `
-    <div class>
+    <div>
       <table>
         <tr>
           <td><h3 contenteditable>${student.name}</h3></td>
@@ -43,16 +44,35 @@ function renderStudentList() {
           <th>Exclude</th>
         </tr>
         <tr>
-          <td contenteditable></td>
-          <td contenteditable></td>
+          <td contenteditable class=js-req-inp>
+            ${student.requests.join("\n")}
+          </td>
+          <td contenteditable class=js-ex-inp>
+            ${student.exclude.join("\n")}
+          </td>
         </tr>
       </table>
-      </div>`;
+    </div>`;
   });
+
   document.getElementById("js-student-list").innerHTML = todoHTML;
   document
     .querySelectorAll(".js-delete-student-button")
     .forEach((jsButton, index) =>
-      jsButton.addEventListener("click", () => removeTodo(index))
+      jsButton.addEventListener("click", () => removeStudent(index))
     );
+
+  // Save settings
+  document.querySelectorAll(".js-req-inp").forEach((entry, idx) => {
+    entry.addEventListener("keyup", () => {
+      studentList[idx].requests = entry.innerHTML.split("\n");
+      localStorage.setItem("student-list", JSON.stringify(studentList));
+    });
+  });
+  document.querySelectorAll(".js-ex-inp").forEach((entry, idx) => {
+    entry.addEventListener("keyup", () => {
+      studentList[idx].exclude = entry.innerHTML.split("\n");
+      localStorage.setItem("student-list", JSON.stringify(studentList));
+    });
+  });
 }
